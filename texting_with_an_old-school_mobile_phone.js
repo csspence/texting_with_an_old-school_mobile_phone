8d64609f6fb6ef01ae8b2eee99ce7f9d8a73bdcf
Crew Spence <csspence123@gmail.com>
2020-03-21 10:30:52 -0500

    Created dictionary to iterate through for solution.

--- texting_with_an_old-school_mobile_phone.js
+++ texting_with_an_old-school_mobile_phone.js
@@ -1,7 +1,10 @@
 /*
-If you're old enough, you might remember buying your first mobile phone, one of the old ones with no touchscreen, and sending your first text message with excitement in your eyes. Maybe you still have one lying in a drawer somewhere.
+If you're old enough, you might remember buying your first mobile phone, one of the old ones with no touchscreen, and sending your 
+first text message with excitement in your eyes. Maybe you still have one lying in a drawer somewhere.
 
-Let's try to remember the good old days and what it was like to send text messages with such devices. A lot of them had different layouts, most notably for special characters and spaces, so for simplicity we'll be using a fictional model with 3x4 key layout shown below:
+Let's try to remember the good old days and what it was like to send text messages with such devices. A lot of them had different 
+layouts, most notably for special characters and spaces, so for simplicity we'll be using a fictional model with 3x4 key layout shown 
+below:
 
 -------------------------
 |   1   |   2   |   3   |  <-- hold a key to type a number
@@ -17,7 +20,9 @@ Let's try to remember the good old days and what it was like to send text messag
 |  '-+= | space |  case |  <-- press # to switch between upper/lower case
 -------------------------
 The task
-You got your thumb ready to go, so you'll receive a message and your job is to figure out which keys you need to press to output the given message with the lowest number of clicks possible. Return the result as a string of key inputs from top row (refer to diagram above).
+You got your thumb ready to go, so you'll receive a message and your job is to figure out which keys you need to press to output the 
+given message with the lowest number of clicks possible. Return the result as a string of key inputs from top row (refer to diagram 
+  above).
 
 Take your time to study the rules below.
 
@@ -26,7 +31,8 @@ Result
 Output string contains inputs that are shown at the top row of a key's layout. (0-9*#)
 
 Typing letters
-To type letters, press a button repeatedly to cycle through the possible characters (bottom row of a key's layout). Pressing is represented by key's top row element repeated n times, where n is the position of character on that particular key. Examples:
+To type letters, press a button repeatedly to cycle through the possible characters (bottom row of a key's layout). Pressing is 
+represented by key's top row element repeated n times, where n is the position of character on that particular key. Examples:
 
 2 => 'a', 9999 => 'z', 111 => '?', *** => '+'
 Typing numbers
@@ -34,41 +40,93 @@ To type numbers 0-9 and special characters *# - hold that key. Holding is repres
 
 3- => '3', 5-5-5- => '555'
 Uppercase / Lowercase
-Initially the case is lowercase. To toggle between lowercase and uppercase letters, use the # symbol. Case switching should only be considered when next character is alphabetic (a-z). Examples:
+Initially the case is lowercase. To toggle between lowercase and uppercase letters, use the # symbol. Case switching should only be 
+considered when next character is alphabetic (a-z). Examples:
 
 #2#9999 => 'Az' (remember, it's a toggle)
 27-#2255 => 'a7BK' (do not switch before '7')
 Waiting for next character
-If you have 2 or more characters in a sequence that reside on the same button (refer to layout, bottom row), you have to wait before pressing the same button again. Waiting is represented by adding a space between 2 (or more) such characters. Example:
+If you have 2 or more characters in a sequence that reside on the same button (refer to layout, bottom row), you have to wait before 
+pressing the same button again. Waiting is represented by adding a space between 2 (or more) such characters. Example:
 
 44 444 44 444 => 'hihi'
-Exceptions: No need to wait after holding any key, even if next character resides on same button (4-4 => '4g'), or if there's a case switch between 2 characters on same button (#5#55 => 'Jk').
+Exceptions: No need to wait after holding any key, even if next character resides on same button (4-4 => '4g'), or if there's a case 
+switch between 2 characters on same button (#5#55 => 'Jk').
 
 Example
 To put it all together, let's go over an example. Say you want to type this message - 'Def Con 1!':
 
 Switch to uppercase with # and press 3 (#3 => D) (input is now in uppercase mode)
 Switch to lowercase and press 3 twice (#33 => e). (Note that there is no waiting because of case switching)
-Next character f is on button 3 again, and has the same case (lowercase input and lowercase character), so you have to wait to type again (' 333' => f).
+Next character f is on button 3 again, and has the same case (lowercase input and lowercase character), so you have to wait to type 
+again (' 333' => f).
 In a similar manner type the second word (space is located on number 0). 0#222#666 660 => ' Con '
-Finish off by holding 1 as 1- and typing ! as 1111 ('1-1111' = 1!). Note that after holding a key you don't need to wait to press another key.
+Finish off by holding 1 as 1- and typing ! as 1111 ('1-1111' = 1!). Note that after holding a key you don't need to wait to press 
+another key.
 Result:
 sendMessage("Def Con 1!") => "#3#33 3330#222#666 6601-1111"
 
-More examples are provided in sample test suite.
 
 All inputs will be valid strings and only consist of characters from the key layout table.
 
 Good luck!
-Also check out other cool katas that inspired this one:
-
-Multi-tap Keypad Text Entry on an Old Mobile Phone by mroth
-Mobile Display Keystrokes by zruF
-Thinking & Testing : Mobile phone simulator by myjinxin2015
 */
 
-const sendMessage = message => {
-  console.log(message);
-  // Start typing here
-  return message;
+const sendMessage = (message) => {
+  let pushes = '';
+  let upperCase = false;
+  const letters = {
+    '.' : one,
+    ',' : one,
+    '?' : one,
+    '!' : one,
+    'a' : two,
+    'b' : two,
+    'c' : two,
+    'd' : three,
+    'e' : three,
+    'f' : three,
+    'g' : four,
+    'h' : four,
+    'i' : four,
+    'j' : five,
+    'k' : five,
+    'l' : five,
+    'm' : six,
+    'n' : six,
+    'o' : six,
+    'p' : seven,
+    'q' : seven,
+    'r' : seven,
+    's' : seven,
+    't' : eight,
+    'u' : eight,
+    'v' : eight,
+    'w' : nine,
+    'x' : nine,
+    'y' : nine,
+    'z' : nine,
+    "'" : star,
+    '-' : star,
+    '+' : star,
+    '=' : star,
+    ' ' : zero
+  }
+  let one = '.,?!';
+  let two = 'abc';
+  let three = 'def';
+  let four = 'ghi';
+  let five = 'jkl';
+  let six = 'mno';
+  let seven = 'pqrs';
+  let eight = 'tuv';
+  let nine = 'wxyz';
+  let star = "'-+=";
+  let zero = ' ';
+
+  // if it is a number
+  // if it is a space
+  // if it is #
+
+  return pushes;
 }
